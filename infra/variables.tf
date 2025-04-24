f9e87f51b40cc6319f009e544205dd8f83db55e5
aforero22 <aforero@xclos.com>
2025-04-24 21:25:26 +0200

    including subnets

--- infra/variables.tf
+++ infra/variables.tf
@@ -13,7 +13,7 @@ variable "vpc_cidr" {
 variable "public_subnets" {
   description = "Lista de subnets públicas"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.10.0/24"]
 }
 
 variable "private_subnets" {
