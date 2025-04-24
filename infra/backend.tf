a7404f001de4760c9194b120e24280b273ecb0dc
aforero22 <aforero@xclos.com>
2025-04-24 20:36:10 +0200

    fixing backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,14 @@
- terraform {
+terraform {
   required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.75"
+    }
+  }
   backend "s3" {
     bucket = "wiz-exercise-terraform-state"
     key    = "state/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-1"  # Literales, no variables
   }
 }
