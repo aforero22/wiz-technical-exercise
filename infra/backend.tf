3d6615b20263ed56c9643da8940139e0e62a9cbf
aforero22 <aforero@xclos.com>
2025-04-24 22:21:29 +0200

    upgrading versions

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = ">= 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.75"
+      version = "~> 5.95"   # o "> = 5.95.0"
     }
   }
   backend "s3" {
