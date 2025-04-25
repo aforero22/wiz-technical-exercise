08114c531cc5f7a6cfcc464bc673320ead81ca85
aforero22 <aforero@xclos.com>
2025-04-25 11:03:11 +0200

    obtaining token

--- infra/main.tf
+++ infra/main.tf
@@ -2,19 +2,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Para aplicar el aws-auth ConfigMap
+# Data sources para obtener detalles del clúster EKS y token de autenticación
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+# Modificar el proveedor Kubernetes para usar los data sources
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = [
-      "eks", "get-token",
-      "--cluster-name", module.eks.cluster_name,
-      "--region",      var.region
-    ]
-  }
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 # VPC y redes
